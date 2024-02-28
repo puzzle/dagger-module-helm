c4a3d2167375b28d4d0887a417cafd7bb49f4fbc
Christoph Raaflaub <raaflaub@puzzle.ch>
2024-02-28 11:24:07 +0100

    document module the new way

--- helm/main.go
+++ helm/main.go
@@ -1,3 +1,7 @@
+// Provides [Helm](https://helm.sh/) functionality as [Dagger Module](https://daggerverse.dev/).
+//
+// Module Home: https://github.com/puzzle/dagger-module-helm
+
 package main
 
 import (
@@ -32,9 +36,18 @@ func (p PushOpts) getRepoFqdn() string {
 	return fmt.Sprintf("%s/%s", p.Registry, p.Repository)
 }
 
-// Read version from Helm Chart
-func (h *Helm) Version(ctx context.Context, directory *Directory) (string, error) {
-	c := dag.Container().From("registry.puzzle.ch/cicd/alpine-base:latest").WithDirectory("/helm", directory).WithWorkdir("/helm")
+// Get and display the version of the Helm Chart located at the directory given by the `--directory` flag.
+//
+// Example usage: dagger call version --directory ./mychart/
+func (h *Helm) Version(
+	// method call context
+	ctx context.Context,
+	// directory that contains the Helm Chart
+	directory *Directory,
+	) (string, error) {
+	c := dag.Container().From("registry.puzzle.ch/cicd/alpine-base:latest").
+		WithDirectory("/helm", directory).
+		WithWorkdir("/helm")
 	version, err := c.WithExec([]string{"sh", "-c", "helm show chart . | yq eval '.version' -"}).Stdout(ctx)
 	if err != nil {
 		return "", err
@@ -44,11 +57,32 @@ func (h *Helm) Version(ctx context.Context, directory *Directory) (string, error
 }
 
 // Package and push an Helm Chart into a registry
+//
 // return true && nil, chart was pushed successfully
 // return false && nil, chart was not pushed. The specified version already exists
 // return true/false && error, an error occurred
-func (h *Helm) PackagePush(ctx context.Context, directory *Directory, registry string, repository string, username string, password string) (bool, error) {
-
+//
+// Example usage:
+// dagger call package-push \
+//   --registry registry.puzzle.ch \
+//   --repository helm \
+//   --username $REGISTRY_HELM_USER \
+//   --password $REGISTRY_HELM_PASSWORD \
+//   --directory ./mychart/
+func (h *Helm) PackagePush(
+	// method call context
+	ctx context.Context,
+	// directory that contains the Helm Chart
+	directory *Directory,
+	// URL of the registry
+	registry string,
+	// name of the repository
+	repository string,
+	// registry login username
+	username string,
+	// registry login password
+	password string,
+) (bool, error) {
 	opts := PushOpts{
 		Registry:   registry,
 		Repository: repository,
@@ -106,14 +140,18 @@ func (h *Helm) PackagePush(ctx context.Context, directory *Directory, registry s
 	return true, nil
 }
 
-// Run helm unit test in a given directory
+
+// Run [Helm unittests](https://github.com/helm-unittest/helm-unittest) with the given directory and files.
+//
+// Provide the helm chart directory with pointing to it with the `--directory` flag.
+// Add the directory location with `"."` as `--args` parameter to tell helm unittest where to find the helm chart with the tests.
+//
+// Example usage: dagger call test --directory ./mychart/ --args "."
 func (h *Helm) Test(
 	// method call context
 	ctx context.Context,
-
-	// local directory that contains the Helm Chart
+	// directory that contains the Helm Chart
 	directory *Directory,
-
 	// Helm Unittest arguments
 	args []string,
 ) (string, error) {
