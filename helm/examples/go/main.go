bef933f8557492e80d2309d1e5065bc7b8e17a6d
Christoph Raaflaub <raaflaub@puzzle.ch>
2024-11-14 21:58:27 +0100

    nicefy package push examples

--- helm/examples/go/main.go
+++ helm/examples/go/main.go
@@ -11,9 +11,16 @@ import (
 
 type Examples struct{}
 
+
+// Example on how to call the PackagePush method.
+// Packages and pushes a Helm chart to a specified OCI-compatible registry with authentication.
+//
+// Return: true if the chart was successfully pushed, or false if the chart already exists, with error handling for push failures.
 func (h *Examples) HelmPackagepush(
 	// method call context
 	ctx context.Context,
+	// directory that contains the Helm Chart
+	directory *dagger.Directory,
 	// URL of the registry
 	registry string,
 	// name of the repository
@@ -22,16 +29,10 @@ func (h *Examples) HelmPackagepush(
 	username string,
 	// registry login password
 	password *dagger.Secret,
-) error {
-	// directory that contains the Helm Chart
-	directory := dag.CurrentModule().Source().Directory("./testdata/mychart/")
-	_, err := dag.Helm().PackagePush(ctx, directory, registry, repository, username, password)
-
-	if err != nil {
-		return err
-	}
-
-	return nil
+) (bool, error) {
+	return dag.
+			Helm().
+			PackagePush(ctx, directory, registry, repository, username, password)
 }
 
 // Example on how to call the Test method.
