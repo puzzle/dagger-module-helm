88c50d42006a94001201e91dd1ec68aa58eaee5f
Sylvain Gilgen <gilgen@puzzle.ch>
2025-09-24 09:43:38 +0200

    print for debugging

--- ci/main.go
+++ ci/main.go
@@ -8,6 +8,7 @@ import (
 	"context"
 	"dagger/ci/internal/dagger"
 	"fmt"
+	"os"
 )
 
 type Ci struct{}
@@ -62,6 +63,8 @@ func (m *Ci) Publish(
 
 	reference := fmt.Sprintf("%s/%s:%s", registry, repository, imageTag)
 
+	fmt.Fprintf(os.Stdout, registry)
+	fmt.Fprintf(os.Stdout, reference)
 	// publish to registry
 	ref, err := container.
 		WithRegistryAuth(registry, username, password).
